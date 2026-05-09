ea37d30932d3fa0758709aefca7b6556901d08b1
Silas124 <silasdanova@gmail.com>
2026-05-09 18:10:40 -0300

    Primeira versão

---
+++ README.md
@@ -0,0 +1,2 @@
+# controle-de-gastos-
+Projeto desenvolvido em Python para automatizar o controle financeiro da empresa utilizando OCR. O sistema permite extrair informações de notas fiscais, recibos e contas através de imagens, organizando automaticamente dados como valor, data e estabelecimento em planilhas para facilitar a gestão financeira diária.
---
+++ renderer.jsnotepad renderer.jsnotepad renderer.jsnotepad renderer.jsnnnnnnnnnnnnnnnotepad renderer.js
@@ -0,0 +1,32 @@
+let total = 0
+
+function adicionarGasto(){
+
+    const descricao = document.getElementById('descricao').value
+
+    const valor = Number(
+        document.getElementById('valor').value
+    )
+
+    if(!descricao || !valor){
+        return
+    }
+
+    const lista = document.getElementById('lista')
+
+    const item = document.createElement('li')
+
+    item.textContent =
+        `${descricao} - R$ ${valor}`
+
+    lista.appendChild(item)
+
+    total += valor
+
+    document.getElementById('total')
+        .textContent = total.toFixed(2)
+
+    document.getElementById('descricao').value = ''
+
+    document.getElementById('valor').value = ''
+}
